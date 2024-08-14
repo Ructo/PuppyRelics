c80361476326f91a0e83c29e9f78c217842d1350
NinjaPuppyGaming <ngruebmeyer@gmail.com>
2024-08-14 10:23:23 -0400

    Unturned Stone Adjustment
    Readjusted to make it a shuffle relic.

--- src/main/java/puppyrelics/relics/UnturnedStone.java
+++ src/main/java/puppyrelics/relics/UnturnedStone.java
@@ -1,84 +1,31 @@
 package puppyrelics.relics;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
-import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
-import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
-import puppyrelics.util.ProAudio;
-
 import static puppyrelics.ModFile.makeID;
 import static puppyrelics.util.Wiz.playAudio;
+import puppyrelics.util.ProAudio;
 
 public class UnturnedStone extends AbstractEasyClickRelic {
     public static final String ID = makeID("UnturnedStone");
-
-    private boolean drawPileTriggered = false;
 
     public UnturnedStone() {
         super(ID, RelicTier.UNCOMMON, LandingSound.FLAT);
     }
 
     @Override
-    public void atBattleStart() {
-        drawPileTriggered = false;
+    public void onShuffle() {
+        triggerEffect();
     }
-
-    @Override
-    public void atTurnStart() {
-        drawPileTriggered = false;
-    }
-
-    @Override
-    public void onCardDraw(AbstractCard drawnCard) {
-        checkHandEmpty();
-    }
-
-    @Override
-    public void onExhaust(AbstractCard card) {
-        checkHandEmpty();
-    }
-
-    @Override
-    public void onManualDiscard() {
-        checkHandEmpty();
-    }
-
-    private void checkHandEmpty() {
-        if (AbstractDungeon.player.hand.isEmpty()) {
-            triggerEffect();
-        }
-    }
-
-    @Override
-    public void update() {
-        super.update();
-
-
-        if (CardCrawlGame.isInARun() && AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT
-                && !AbstractDungeon.actionManager.turnHasEnded) {
-
-
-            if (!drawPileTriggered && AbstractDungeon.player.drawPile.isEmpty()) {
-                triggerEffect();
-                drawPileTriggered = true;
-            }
-        }
-    }
-
 
     private void triggerEffect() {
-        AbstractMonster target = AbstractDungeon.getMonsters().getRandomMonster(true);
-        if (target != null) {
-            flash();
-            AbstractDungeon.actionManager.addToBottom(new DamageAction(target, new DamageInfo(AbstractDungeon.player, 5, DamageInfo.DamageType.THORNS), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
-        }
+        flash();
+        AbstractDungeon.actionManager.addToBottom(new DamageAllEnemiesAction(
+                AbstractDungeon.player, 8, DamageInfo.DamageType.THORNS, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
     }
 
     @Override
